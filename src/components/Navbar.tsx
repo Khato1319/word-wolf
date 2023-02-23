4af7fc08ef5fccf046e811f186f98a55c0ffb24e
khcatino <kcatino@itba.edu.ar>
2023-02-23 20:15:14 -0300

    First commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app'
+
+export default function Navbar() {
+    return <header className='bg-blue-500 w-full h-12'>
+      Hola
+    </header>
+  }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@component/styles/globals.css'
+import '/src/styles/globals.css'
+import Navbar from '@components//Navbar'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className='w-full min-h-screen flex flex-col'>
+    <Navbar></Navbar>
+    <main className='w-full flex-1 flex'>
+    <Component {...pageProps} />
+    </main>
+  </div>
 }
