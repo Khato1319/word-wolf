4b35c4182ab402b85542ecd556690e31536f7f96
khcatino <kcatino@itba.edu.ar>
2023-02-25 17:17:52 -0300

    Uncomment api call to OpenAI (now owrking correctly)

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import { workerData } from "worker_threads";
 
 export class Api {
     static async getWords() {
-        return ['hola', 'chau']
+        // return ['hola', 'chau']
         const r = await axios.post('https://api.openai.com/v1/completions',{"model": "text-davinci-003", "prompt": "Give me two words (they can be nouns, actions, activities) separated by a comma that are semantically similar but not synonyms. Just give me the 2 words without any other text surrounding it.", "temperature": 0.9, "max_tokens": 7},{
             headers: {
                 Authorization: `Bearer ${API_KEY}`
