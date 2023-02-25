fd27e1e24e783b54c27999d9ad8e68f4daade9ab
khcatino <kcatino@itba.edu.ar>
2023-02-25 17:13:29 -0300

    Add homepage link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 export default function Navbar() {
     return <header className='bg-blue-500 w-full h-12 flex items-center'>
-      <div className='px-2 font-bold'>
+      <Link href={'/'} className='px-2 font-bold'>
       WordWolf
-      </div>
+      </Link>
     </header>
   }
