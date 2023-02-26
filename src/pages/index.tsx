0cd288bf44a51111adce6f15e6312f973d367f7f
khcatino <kcatino@itba.edu.ar>
2023-02-26 20:24:48 -0300

    Add instructions to /play route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex justify-center items-center flex-col flex-1'>
-       <h1>
-        WordWolf
+      <div className='flex justify-center items-center flex-col flex-1 gap-2'>
+       <h1 className='text-2xl'>
+        Welcome to WordWolf!
        </h1>
-       <Link href='/play' className='bg-red-300 rounded-sm p-1'>Play!</Link>
+       <h2 className='text-xl'>How to play:</h2>
+       <div>
+        <ol>
+        <li>
+        1. Every person will be assigned a word
+
+        </li>
+        <li>
+           
+        2. Only one of the participants will be given a different word (that person will be the <a className='italic font-bold'>wolf</a>)
+        </li>
+        <li>
+          3. When the timer begins, the players will start talking about the word they were given, and the goal is to discover who the wolf is.
+        </li>
+        <li>
+          4. At the end of the round everyone will get to vote who they think the wolf is
+        </li>
+        <li>
+          5. If the majority correctly votes for the wolf then the <a className='italic font-bold'>people</a> win, otherwise the <a className='italic font-bold'>wolf</a> wins.
+        </li>
+        </ol>
+   
+       </div>
+       <Link href='/play' className='bg-red-300 rounded-sm p-1 text-lg'>Play!</Link>
       </div>
     </>
   )
