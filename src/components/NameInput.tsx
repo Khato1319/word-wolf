8e37371bda75c25f09e045b24edbc18d25c9d2be
khcatino <kcatino@itba.edu.ar>
2023-02-26 20:25:18 -0300

    Update NameInput styles

--- src/components/NameInput.tsx
+++ src/components/NameInput.tsx
@@ -7,5 +7,5 @@ export default function NameInput({text, onChange}:{
   text: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
 }) {
-    return <input value={text} onChange={onChange} className='bg-black rounded-md w-1/2 max-w-md h-12 m-2 p-1'/>
+    return <input value={text} onChange={onChange} className='bg-gray-400 outline-0 rounded-md w-1/2 max-w-md h-12 m-2 p-1 px-2'/>
   }
